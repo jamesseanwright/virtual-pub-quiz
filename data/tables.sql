f06b025fb6fa8f0960875d4189d7779f8d4f4fa3
James Wright <james@jamesswright.co.uk>
2020-04-18 22:42:49 +0100

    Fixes table creation script

--- data/tables.sql
+++ data/tables.sql
@@ -6,22 +6,22 @@
 
 create schema if not exists pubquiz;
 
-create table if not exists pubquiz.category (
-  id uuid primary key not null,
-  display_name text not null
-  questions uuid not null references pubquiz.category_questions(category_id)
-);
-
-create table if not exists pubquiz.question (
-  id uuid primary key not null,
-  contents text not null,
-  correct_answer uuid not null references pubquiz.answer(id),
-  possible_answers uuid not null references pubquiz.possible_answers(question_id)
-);
-
 create table if not exists pubquiz.category_questions (
   id uuid primary key not null,
+  category_id uuid not null,
+  question_id uuid not null
+);
+
+create table if not exists pubquiz.possible_answers (
+  id uuid primary key not null,
   question_id uuid not null,
+  answer_id uuid not null
+);
+
+create table if not exists pubquiz.category (
+  id uuid primary key not null,
+  display_name text not null,
+  questions uuid not null references pubquiz.category_questions(id)
 );
 
 create table if not exists pubquiz.answer (
@@ -29,8 +29,9 @@ create table if not exists pubquiz.answer (
   contents text not null
 );
 
-create table if not exists pubquiz.possible_answer (
+create table if not exists pubquiz.question (
   id uuid primary key not null,
-  question_id uuid not null,
-  answer_id uuid not null
+  contents text not null,
+  correct_answer uuid not null references pubquiz.answer(id),
+  possible_answers uuid not null references pubquiz.possible_answers(id)
 );
