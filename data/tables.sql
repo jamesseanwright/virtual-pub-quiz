06d9d36f598c8a9636423115ff799ad86e6e216b
James Wright <james@jamesswright.co.uk>
2020-04-18 23:48:03 +0100

    Sets foreign key constraint on link tables

--- data/tables.sql
+++ data/tables.sql
@@ -6,22 +6,10 @@
 
 create schema if not exists pubquiz;
 
-create table if not exists pubquiz.category_questions (
-  id uuid primary key not null,
-  category_id uuid not null,
-  question_id uuid not null
-);
-
-create table if not exists pubquiz.possible_answers (
-  id uuid primary key not null,
-  question_id uuid not null,
-  answer_id uuid not null
-);
-
 create table if not exists pubquiz.category (
   id uuid primary key not null,
   display_name text not null,
-  questions uuid not null references pubquiz.category_questions(id)
+  questions uuid not null
 );
 
 create table if not exists pubquiz.answer (
@@ -32,6 +20,18 @@ create table if not exists pubquiz.answer (
 create table if not exists pubquiz.question (
   id uuid primary key not null,
   contents text not null,
-  correct_answer uuid not null references pubquiz.answer(id),
-  possible_answers uuid not null references pubquiz.possible_answers(id)
+  correct_answer uuid not null
+  possible_answers uuid not null
+);
+
+create table if not exists pubquiz.category_questions (
+  id uuid primary key not null,
+  category_id uuid references pubquiz.category(id),
+  question_id uuid references pubquiz.question(id)
+);
+
+create table if not exists pubquiz.possible_answers (
+  id uuid primary key not null,
+  question_id uuid references pubquiz.question(id),
+  answer_id uuid references pubquiz.answer(id)
 );
