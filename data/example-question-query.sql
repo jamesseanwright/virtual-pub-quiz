017d3958893f2d3e21a0dd2fbc96e7064ab1d41a
James Wright <james@jamesswright.co.uk>
2020-04-19 21:58:16 +0100

    Updates example get question query to build JSON aggregation for possible answers with ID and contents

--- data/example-question-query.sql
+++ data/example-question-query.sql
@@ -1,6 +1,10 @@
-select pubquiz.question.id, contents, correct_answer, array_agg(answer_id) as possible_answers
-from pubquiz.question
-inner join pubquiz.possible_answers
-on pubquiz.question.id = pubquiz.possible_answers .question_id
-where pubquiz.question.id = 'c78e3ff0-fa3b-4180-a3dd-2fafce95cf9c'
-group by pubquiz.question.id
+set schema 'pubquiz';
+
+select question.id, question.contents, json_agg(json_build_object('id', answer.id, 'contents', answer.contents)) as possible_answers
+from question
+inner join possible_answers
+on question.id = possible_answers.question_id
+inner join answer
+on answer.id = possible_answers.answer_id
+where question.id = 'c78e3ff0-fa3b-4180-a3dd-2fafce95cf9c'
+group by question.id
